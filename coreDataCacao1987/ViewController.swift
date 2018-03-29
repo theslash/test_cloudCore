6e44e3898dd7342f1f4cd3c4a027579b75f98d18
Rudi Krämer <rudi.kraemer@papercuts.de>
2018-03-29 12:09:38 +0200

    Initial Commit

---
+++ coreDataCacao1987/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coreDataCacao1987
+//
+//  Created by Rudi Krämer on 29.03.18.
+//  Copyright © 2018 Rudi Krämer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
