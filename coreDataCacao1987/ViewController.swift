0c345b7a2161a0179630488b420a3d83db99f006
Rudi Krämer <rudi.kraemer@papercuts.de>
2018-03-29 12:20:30 +0200

    .

--- coreDataCacao1987/ViewController.swift
+++ coreDataCacao1987/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var people = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onPlusTabbed() {
+        
     }
-
-
+    
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell (style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = ""
+        
+        return cell
+    }
+    
+}
