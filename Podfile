d7efc230b81b31eb6d321e36c835be233c01552a
Rudi Krämer <rudi.kraemer@papercuts.de>
2018-03-29 21:11:03 +0200

    cocoapod

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '10.0'
+
+target 'coreDataCacao1987' do
+  use_frameworks!
+
+pod 'CloudCore', '~> 2.0'
+
+
+end
---
+++ Pods/Target Support Files/Pods-coreDataCacao1987/Pods-coreDataCacao1987-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_coreDataCacao1987 : NSObject
+@end
+@implementation PodsDummy_Pods_coreDataCacao1987
+@end
